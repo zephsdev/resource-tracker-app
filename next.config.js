bc2fe3a21694d049a6596e1349f79fbfcf170da9
Zephs <zephsdev@gmail.com>
2025-06-06 14:56:58 +0100

    this is it

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: { unoptimized: true },  // optional but recommended for static
 };
 
 module.exports = nextConfig;
