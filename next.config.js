7c3ba3cbe7bd7e879b95d2b808b1218351b5bd93
Zephs <zephsdev@gmail.com>
2025-06-06 14:52:27 +0100

    confused about the changes made in this commit? Here are some details:

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
-}
+  output: 'export',
+  images: { unoptimized: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
